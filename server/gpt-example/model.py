6d7aef8c53a4d79521c3a5b3d2192f714e6349da
sindhuso <somasundaram.sindhu@gmail.com>
2023-04-19 19:58:14 -0700

    Remove types

--- server/gpt-example/model.py
+++ server/gpt-example/model.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple, Optional, TypeVar, Type
 from transformers import PreTrainedTokenizerBase
 
-from text_generation_server.models.types import Batch, GeneratedText
+from parameters import GeneratedText, Batch
 
 B = TypeVar("B", bound=Batch)
 
